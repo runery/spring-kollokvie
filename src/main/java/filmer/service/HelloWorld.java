b1c6599f9048a714d2380b6a1d30769c2d3438dc
runery <runery@gmail.com>
2013-10-17 07:52:38 +0200

    Enkel HelloWorld klasse for å sjekke at det fungerer

---
+++ src/main/java/filmer/service/HelloWorld.java
@@ -0,0 +1,14 @@
+package filmer.service;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rry
+ * Date: 17.10.13
+ * Time: 07:47
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hei Verden!");
+    }
+}
